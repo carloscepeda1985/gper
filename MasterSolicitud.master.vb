870333fa47b01d80d0cb22d0a21e7ea9b23cb014
carloscepeda1985 <carlos@yousoft.cl>
2020-02-02 17:49:54 -0300

    Se agrega cerrar sesion a perfil contratista

--- MasterSolicitud.master.vb
+++ MasterSolicitud.master.vb
@@ -2,5 +2,8 @@
 Partial Class MasterSolicitud
     Inherits System.Web.UI.MasterPage
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Response.Redirect("Default.aspx")
+    End Sub
 End Class
 
